64b18f0ce62ca7e1f6f97d3c439ccaa2fa892b15
pankaj-anadure <pankaj-anadure@wissen.com>
2022-03-14 12:47:16 +0530

    Altered Abc.java

--- Abc.java
+++ Abc.java
@@ -2,7 +2,7 @@ public class Abc
 {
 	public static void main(String args[])
 	{
-		for(int i=1; i<=10; i++)
-			System.out.println("HELLO");
+		for(int i=1; i<=20; i++)
+			System.out.println("HELLO : "+i);
 	}
 }
