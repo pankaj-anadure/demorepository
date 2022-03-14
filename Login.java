6186d4305ec693ac9a4139abf83b3b52f13067d7
pankaj-anadure <pankaj-anadure@wissen.com>
2022-03-14 17:52:42 +0530

    SSO option aded

--- Login.java
+++ Login.java
@@ -2,6 +2,7 @@ public class Login
 {
 	public void login()
 	{
-		System.out.println("Now you can login");
+		System.out.println("Now you can login using GMail ID");
+		System.out.println("Now you can login using Facebook Account");
 	}
 }
